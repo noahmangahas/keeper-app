b269574145418de1e1a8de9e18ba2e77643cb171
Noah Mangahas <noah.mangahas@gmail.com>
2021-04-14 17:37:28 +1000

    Updates

---
+++ precache-manifest.25f69e2276eefaedfd795c79c4b9b2ef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd7dec7345fdbd159b85d15924f7c81",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "221fd50f921234da8247",
+    "url": "/my-app/static/js/2.f62d1186.chunk.js"
+  },
+  {
+    "revision": "39c52752c70ca796e952",
+    "url": "/my-app/static/js/main.a09c117f.chunk.js"
+  },
+  {
+    "revision": "31df889f9209a2d58b9c",
+    "url": "/my-app/static/js/runtime-main.a42d48cd.js"
+  }
+]);
